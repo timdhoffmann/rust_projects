d7224773f53267372df720bfa0b089414fb2c170
timdhoffmann <9947384+timdhoffmann@users.noreply.github.com>
2023-09-03 14:05:04 +0200

    Require, and handle, one or more arguments.

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -4,16 +4,18 @@ use clap::Parser;
 // Read values from `Cargo.toml`
 #[command(author, version, about, long_about = None)]
 struct Args {
-    text: String,
+    #[arg(required = true)]
+    text: Vec<String>,
 
     /// Don't append a newline character.
     #[arg(short = 'n', long)]
     omit_newline: bool
 }
+
 fn main() {
     let args = Args::parse();
 
     let ending = if args.omit_newline {""} else {"\n"};
-    let output = args.text + ending;
-    print!("{}", output)
+    let output = args.text.join(" ");
+    print!("{}{}", output,ending)
 }
--- echor/tests/cli.rs
+++ echor/tests/cli.rs
@@ -2,7 +2,8 @@ use assert_cmd::Command;
 use predicates::prelude::*;
 use std::fs;
 
-// An 'alias type' used to return the unit type, or an error.
+// An 'alias type' used to return the unit type, or an error,
+// in failure cases, e.g. when a file to read doesn't exist.
 type TestResult = Result<(), Box<dyn std::error::Error>>;
 
 #[test]
@@ -11,6 +12,7 @@ fn dies_without_arguments() -> TestResult {
     // an 'Err'.
     let mut cmd = Command::cargo_bin("echor")?;
     let message = "required arguments were not provided".to_string();
+
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains(message));
@@ -23,7 +25,19 @@ fn hello1() -> TestResult {
     let outfile = "tests/expected/hello1.txt";
     let expected = fs::read_to_string(outfile)?;
     let mut cmd = Command::cargo_bin("echor")?;
-    cmd.arg("Hello there")
+
+    cmd.arg("Hello there").assert().success().stdout(expected);
+
+    Ok(())
+}
+
+#[test]
+fn hello2() -> TestResult {
+    let outfile = "tests/expected/hello2.txt";
+    let expected = fs::read_to_string(outfile)?;
+    let mut cmd = Command::cargo_bin("echor")?;
+
+    cmd.args(vec!["Hello", "there"])
         .assert()
         .success()
         .stdout(expected);
